f0cd34ee37666bee9c3a79ee07f53f60747229b2
orxan905 <orxan.huseynli2002@mail.ru>
2022-12-05 06:52:43 -0800

    hazirdir

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+
+
+function About() {
+  return (
+    <article>
+      <h1>It's about me</h1>
+    </article>
+  );
+}
+
+function Work() {
+  return (
+    <article>
+      <h1>Our work is not response.</h1>
+    </article>
+  );
+}
+
+function Contact() {
+  return (
+    <article>
+      <h1>Phone is not found</h1>
+    </article>
+  );
+}
+
+function Nav() {
+  return (
+    <header>
+      <nav className="nav">
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+        <Link className="nav-link" to="/work">
+          Works
+        </Link>
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+function Page() {
+  return (
+    <main>
+      <Routes>
+        <Route path="/about" element={<About />}/>
+      <Route path="/work" element={<Work />}/>
+      <Route path="/contact" element={<Contact />}/>
+      </Routes>
+      
+        
+    </main>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Page />
+    </BrowserRouter>
+  );
+}
+
+export default App;
